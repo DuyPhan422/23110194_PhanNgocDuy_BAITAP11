2187b5ccd98200cbbc10a269c119ae00184606b9
DuyPhanZ <phanngocduypnd@gmail.com>
2025-10-03 23:06:47 +0700

    full bt 11

---
+++ demoSringbootST5ST6-8/src/main/java/vn/iotstar/DemoSringbootSt5St68Application.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSringbootSt5St68Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSringbootSt5St68Application.class, args);
+	}
+
+}
